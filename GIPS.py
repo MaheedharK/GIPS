60b10f751225c5156766f17abc4ee4950b160ea2
MaheeDrax <45009078+MaheedharK@users.noreply.github.com>
2021-04-10 11:03:48 +0530

    Update GIPS.py

--- GIPS.py
+++ GIPS.py
@@ -5,14 +5,19 @@ import csv
 import getmac
 import os
 import threading
+import ipaddress
+
+import pprint
+from netaddr import *
 import requests
 from queue import Queue
 from scapy.all import *
 from nmb.NetBIOS import NetBIOS
 
+
 class LANnetworks(object):
     def __init__(self):
-        ip=input("plz enter d ip of router is 192.168.43.1/192.168.0.1")
+        ip=input("plz enter d ip of router is 192.168.0.1/192.168.0.1")
         self.ip=ip
     def networkscanner(self):
         if len(self.ip)==0:
@@ -29,12 +34,12 @@ class LANnetworks(object):
             hosts.append(host)
             time.sleep(15)
             
-            time.sleep(15)
+            time.sleep(5)
         
         i=1
         with open("Network.csv","w",newline="") as file:
              writer=csv.writer(file)
-             writer.writerow(["Router ip","HOST IP","HOST NAME","MAC Address","NetBiosname","Manufacturer","OS"])   
+             writer.writerow(["Router ip","HOST IP","HOST NAME","MAC Address","NetBiosname","Manufacturer","OS","net mask","subnet","Ports open"])   
              while i<len(hosts):
                     
                
@@ -45,7 +50,13 @@ class LANnetworks(object):
                 nbname = n.queryIPForName(hosts[i])
                 print(nbname)
                 
+                ip = IPNetwork(str(hosts[i]))
+                print(hosts[i]," has net mask ",ip.netmask)
+                
+                net = ipaddress.ip_network('192.168.43.1/255.255.255.0', strict=False)
+                print(net.network_address)
                 time.sleep(20)
+                
                 ttl_values = {32: "Windows", 60: "MAC OS", 64: "MAC OS / Linux", 128: "Windows", 255: "Linux 2.4 Kernal"}
                 ans = sr1(IP(dst=str(hosts[i])) / ICMP(), timeout = 2, verbose=0)
                 if ans:
@@ -61,19 +72,59 @@ class LANnetworks(object):
                    
                 host_name=socket.gethostbyaddr(hosts[i])
                 print("host ip is",hosts[i],"host name is",host_name[0])
-                netbios
+                
                 time.sleep(10)
-                
-                
+                ##port scan
+                socket.setdefaulttimeout(0.25)
+                print_lock = threading.Lock()
+                Total_ports_open = []   
+                target = str(hosts[i])
+                t_IP = socket.gethostbyname(target)
+                print ('Starting scan on host: ', t_IP)
+                def portscan(port):
+                  s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                  try:
+                   con = s.connect((t_IP, port))
+                   with print_lock:
+                    print(port, 'is open')
+                    Total_ports_open.append(port)
+                   con.close()
+                  except:
+                    pass
+                def threader():
+                  while True:
+                   worker = q.get()
+                   portscan(worker)
+                   q.task_done()
+   
+                q = Queue()
+                startTime = time.time()
+   
+                for x in range(100):
+                 t = threading.Thread(target = threader)
+                 t.daemon = True
+                 t.start()
+
+                for worker in range(1, 5000):
+                   q.put(worker)
+                q.join()
+
+                print('Time taken:', time.time() - startTime)
+                print(Total_ports_open)
                
                 
-                writer.writerow([hosts[0],hosts[i],host_name[0],getmac.get_mac_address(ip=hosts[i], network_request=True), n.queryIPForName(hosts[i]),vendor,ttl_values.get(ans.ttl)])
+                writer.writerow([hosts[0],hosts[i],host_name[0],getmac.get_mac_address(ip=hosts[i], network_request=True), n.queryIPForName(hosts[i]),vendor,ttl_values.get(ans.ttl),ip.netmask,net.network_address,Total_ports_open])
+                 
+                
+               
                  
               except:
                 pass
+               
               i+=1
-             
 if __name__=="__main__":
     D=LANnetworks()
     D.networkscanner()
   
+    
+ 
